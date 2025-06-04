81c804c08a29b1f61e5cf08b5fbf64c6750151da
GabrielPertelCorti <gabrielpertel3007@gmail.com>
2025-06-04 15:13:22 -0300

    Create aluno.h

---
+++ trab1/aluno.h
@@ -0,0 +1,19 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+typedef struct {
+    int matricula;
+    char nome[50];
+    char turma[6];
+    float nota[4];
+} Aluno;
+
+void limparBuffer();
+int matriculaExiste(const char *filename, int matricula);
+void cadastrarAluno(FILE *arquivo);
+void mostrarDados();
+void relatorioTurma();
+void relatorioAluno();
+void listarAlunosPorTurma();
+
+#endif
